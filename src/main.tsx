28e1d86a9c60e21add0437af8c7639549152dc36
TimHL5 <liucgz@bc.edu>
2025-11-10 07:17:02 -0500

    Web Analytics Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'  // ← KEEP THIS
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>  {/* ← NEED THIS */}
+      <App />
+      <Analytics />
+    </BrowserRouter>  {/* ← NEED THIS */}
+  </React.StrictMode>,
 )
