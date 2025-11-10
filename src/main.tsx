3007c0db6e60d02ce1d4c7d7fecba08052043686
TimHL5 <liucgz@bc.edu>
2025-11-10 07:18:15 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'  // ← ADD THIS
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <Analytics />  {/* ← ADD THIS */}
+    </BrowserRouter>
+  </React.StrictMode>,
 )
